f992e885ef424b057e2ca055c414db678b98c702
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2025-06-15 16:05:10 +0300

    Fix log message in PrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -24,7 +24,7 @@ export class PrismaService
   }
 
   async onModuleDestroy() {
-    this.logger.log('[PrismaService] Disconnecting to database...');
+    this.logger.log('[PrismaService] Disconnecting from database...');
 
     await this.$disconnect();
   }
