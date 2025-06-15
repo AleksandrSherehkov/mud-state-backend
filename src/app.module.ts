95db3b9a6514af4f22ac33e86c5ec74060f18a22
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2025-06-15 12:09:21 +0300

    feat(config): add Joi validation schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,23 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        JWT_ACCESS_SECRET: Joi.string().required(),
+        JWT_REFRESH_SECRET: Joi.string().required(),
+        PORT: Joi.number().default(3000),
+        BASE_URL: Joi.string().uri().default('http://localhost:3000'),
+        JWT_ACCESS_EXPIRES_IN: Joi.string().default('15m'),
+        JWT_REFRESH_EXPIRES_IN: Joi.string().default('7d'),
+        BCRYPT_SALT_ROUNDS: Joi.number().default(10),
+      }),
     }),
     ScheduleModule.forRoot(),
     PrismaModule,
